c7b290e36c31d778a0acb71d518f6ef769bc6866
Jwan Mardini <98626283+JwanMardini@users.noreply.github.com>
2023-04-25 00:44:42 +0200

    Added an example of javaFX GUI

--- GUI_Exampel/SwingProject/src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        loginFrame frame = new loginFrame();
-    }
-}
---
+++ JavaFXEX/src/Main.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    private static Stage stg;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stg = stage;
+        stage.setResizable(false);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stage.setTitle("Sleep-Tracker");
+        stage.setScene(new Scene(root, 600, 400));
+        stage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException{
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
+    }
+}
