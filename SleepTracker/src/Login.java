03e21bc7ae2ee846c9e13a1d0278f176e108758c
Jwan Mardini <98626283+JwanMardini@users.noreply.github.com>
2023-04-25 18:48:04 +0200

    Login GUI is created

---
+++ SleepTracker/src/Login.java
@@ -0,0 +1,37 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class Login {
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private CheckBox checkBox;
+
+    @FXML
+    private Hyperlink forgotPassword;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button createAccount;
+
+
+    public void userLogin(ActionEvent event){
+
+    }
+
+    public void createAccount(ActionEvent event){
+
+    }
+
+    public void forgotPassword(ActionEvent event){
+
+    }
+
+}
