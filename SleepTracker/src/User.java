150fbef4eae9a1c3a0da73211d4daee1b1e310fe
Jwan Mardini <98626283+JwanMardini@users.noreply.github.com>
2023-04-27 17:24:56 +0200

    "Added one class for user info, and one for DB connection"

---
+++ SleepTracker/src/User.java
@@ -0,0 +1,39 @@
+public class User {
+    int id = 0;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
