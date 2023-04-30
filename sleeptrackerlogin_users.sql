edb96ef60c8b759d902bceda05ae9788a19e8bbd
Jwan Mardini <98626283+JwanMardini@users.noreply.github.com>
2023-04-30 18:04:32 +0200

    Added the email column to the users table in databas file

--- sleeptrackerlogin_users.sql
+++ sleeptrackerlogin_users.sql
@@ -26,6 +26,7 @@ CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `Password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
