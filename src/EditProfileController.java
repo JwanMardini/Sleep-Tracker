67813dbd5e2be59017ad5deef3b30187d5b7c5d3
Jwan Mardini <98626283+JwanMardini@users.noreply.github.com>
2023-05-09 11:42:53 +0200

    Added ID to the components

--- src/EditProfileController.java
+++ src/EditProfileController.java
@@ -1,6 +1,8 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 import java.io.IOException;
 
 
@@ -10,9 +12,25 @@ public class EditProfileController {
     private Button btn_back_to_home;
 
     @FXML
+    private Button saveButton;
+
+    @FXML
+    private TextField nameID;
+
+    @FXML
+    private TextField usernameID;
+
+    @FXML
+    private TextField emailID;
+
+
+
+    @FXML
     void btnBackToHome(ActionEvent event) throws IOException {
         Main.changeScene("resources/logged-in.fxml");
 
     }
+
+
 
 }
