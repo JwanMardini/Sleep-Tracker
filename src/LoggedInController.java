001d55a94faa7af443f0dd509ce9c8b54c6b8d9a
omar zarifa <omarzarifa@gmail.com>
2023-05-12 01:16:39 +0200

    Use the getConnection() in LoggedInController

--- src/LoggedInController.java
+++ src/LoggedInController.java
@@ -98,7 +98,7 @@ public class LoggedInController implements Initializable {
     public void handleSaveButton(ActionEvent actionEvent) {
         try {
 
-            try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sleeptrackerlogin", "root", "toor");
+            try (Connection connection = DBUtils.getConnection();
                  PreparedStatement psGetUserId = connection.prepareStatement("SELECT id FROM users WHERE username = ?");
                  PreparedStatement psInsertDateTime = connection.prepareStatement("INSERT INTO DateTime(start_date, start_time, end_date, end_time, duration, user_id) VALUES (?, ?, ?, ?, ?, ?)")) {
 
@@ -145,7 +145,7 @@ public class LoggedInController implements Initializable {
 
 
     public void displayChart() {
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sleeptrackerlogin", "root", "toor");
+        try (Connection connection = DBUtils.getConnection();
              PreparedStatement psChartSql = connection.prepareStatement("SELECT end_date, SUM(duration) FROM DateTime GROUP BY end_date ORDER BY TIMESTAMP(end_date) ASC LIMIT 8");
              ResultSet rs = psChartSql.executeQuery()) {
 
